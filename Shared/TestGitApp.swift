910b3580382b74301f6b485eeb0c27eb03e780d5
Raymond Chen <ahrchen@gmail.com>
2022-01-31 14:19:21 -0800

    Initial Commit

---
+++ Shared/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  Shared
+//
+//  Created by Raymond Chen on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
